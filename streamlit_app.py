5127522b59627836b425a70fd27ff7291cb46f7a
LeHarkins <145674045+LeHarkins@users.noreply.github.com>
2023-09-21 13:28:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.subtitle ("Breakfast Menu")
-streamlit ("omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled Free-Range Egg")
+streamlit.title ("Breakfast Menu")
+streamlit.title ("omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled Free-Range Egg")
